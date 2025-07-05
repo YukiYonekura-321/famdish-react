2264059e452b54c1009e395b372e156a2ed77b8f
YukiYonekura <yoneyuu321@gmail.com>
2025-07-05 15:44:21 +0900

    💄 🚧  CSS modules追加(スタイルファイルの分割)

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 "use client";
 
+import Style from "@/styles/Home.module.css";
 import { Footer } from "@/components/footer";
 import { Main } from "@/components/main";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
+    <div className={Style.main}>
+    {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div> */}
       <Main page = "home"/>
 
       <Footer />
--- components/headline.js
+++ components/headline.js
@@ -1,3 +1,5 @@
+import style from "@/components/Headline.module.css";
+
 export function Headline(props) {
 
     return (
@@ -6,7 +8,7 @@ export function Headline(props) {
                 Welcome to the {props.page} Page
             </h1>
             {props.paragraph}
-            <p className="text-sm text-gray-500 mt-2">
+            <p className={style.p}>
                 Let's make your {props.page} page
             </p>
             <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded" onClick={props.onClick}>
