3234e82507962df8d027f7f46b247aa2a6620543
YukiYonekura <yoneyuu321@gmail.com>
2025-07-06 09:30:18 +0900

    ✨ ヘッダーの追加

--- app/page.js
+++ app/page.js
@@ -3,11 +3,14 @@
 import Style from "@/styles/Home.module.css";
 import { Footer } from "@/components/footer";
 import { Main } from "@/components/main";
+import { Header } from "@/components/header";
 
 export default function Home() {
   return (
     <div className={Style.main}>
-    {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div> */}
+
+      <Header />
+
       <Main page = "home"/>
 
       <Footer />
---
+++ components/header.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export function Header(){
+    return (
+        <header>
+            <Link href="/">
+                Index
+            </Link>
+
+            <Link href="/about">
+                About
+            </Link>
+        </header>
+    );
+}
