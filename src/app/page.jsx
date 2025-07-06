bfd1b0e39475045491954f67ee31b5d6c89331b5
YukiYonekura <yoneyuu321@gmail.com>
2025-07-06 15:08:51 +0900

    ✨ useEffectを導入

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,20 @@ import Style from "@/src/styles/Home.module.css";
 import { Footer } from "@/src/components/footer.jsx";
 import { Main } from "@/src/components/main.jsx";
 import { Header } from "@/src/components/header.jsx";
+import { useEffect } from "react";
 
 export default function Home() {
+  // console.log("レンダリング！");
+
+  useEffect(() => {
+    console.log("マウント！");
+    document.body.style.backgroundColor = "lightblue"; // 背景色を変更
+    return () => {
+      console.log("アンマウント");
+      document.body.style.backgroundColor = ""; // 背景色を元に戻す
+    };
+  }, []);
+
   return (
     <div className={Style.main}>
 
